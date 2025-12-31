dcd96f7d129655b196d76a5a2493d9308cf91cf2
user1 <user1@DESKTOP-IPL4B0T>
2025-12-31 17:51:19 +0900

    251231(수) 오후

---
+++ Homeworks3_ObjectArray/src/com/kh/hw/member/controller/MemberController.java
@@ -0,0 +1,20 @@
+package com.kh.hw.member.controller;
+
+import com.kh.hw.member.model.vo.Member;
+
+public class MemberController {
+	private Member[] m = new Member[SIZE];
+	public static final int SIZE = 10;
+	
+	
+	public int existMemberNum() {
+		
+	}
+	
+	public boolean checkId(String inputId) {
+		
+	}
+	
+	public void inserMember(String id, )
+	
+}
---
+++ Homeworks3_ObjectArray/src/com/kh/hw/member/model/vo/Member.java
@@ -0,0 +1,80 @@
+package com.kh.hw.member.model.vo;
+
+public class Member {
+	private String id;
+	private String name;
+	private String password;
+	private String email;
+	private char gender;
+	private int age;
+
+	// 기본 생성자
+	public Member() {
+
+	}
+
+	// 6개의 초기값을 받는 매개변수 있는 생성자
+	public Member(String id, String name, String password, String email, char gender, int age) {
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.email = email;
+		this.gender = gender;
+		this.age = age;
+
+	}
+	//필드의 값을 초기화/변경하는 메소드
+	public void setId(String id) {
+		this.id = id;
+	}
+	//필드의 값을 리턴하는 메소드
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String inform() {
+		// Member 클래스의 모든 필드값들을 합쳐 반환
+		return id + ", " + name + ", " + password + ", " + email + ", " + gender + ", " + age;
+	}
+
+}

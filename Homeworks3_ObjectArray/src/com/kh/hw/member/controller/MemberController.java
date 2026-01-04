78242056e35938023309d16f01281c229ef7c693
박무혁 <@DESKTOP-ORTTVS0>
2026-01-05 01:19:27 +0900

    20260105 도선생

--- Homeworks3_ObjectArray/src/com/kh/hw/member/controller/MemberController.java
+++ Homeworks3_ObjectArray/src/com/kh/hw/member/controller/MemberController.java
@@ -3,73 +3,115 @@ package com.kh.hw.member.controller;
 import com.kh.hw.member.model.vo.Member;
 
 public class MemberController {
-    public static final int SIZE = 10;
-    private Member[] m = new Member[SIZE];
+	public static final int SIZE = 10;
+	private Member[] m = new Member[SIZE];
 
-    // 현재 존재하는 멤버 수 반환
-    public int existMemberNum() {
+	// 현재 존재하는 멤버 수 반환
+	public int existMemberNum() {
 
-        int num = 0;
+		int num = 0;
 
-        for(int i= 0; i < m.length; i++) {
-            if(m[i] != null) {
-                num++;
-            }
-        }
-        return num;
-    }
+		for (int i = 0; i < m.length; i++) {
+			if (m[i] != null) {
+				num++;
+			}
+		}
+		return num;
+	}
 
-    public boolean checkId(String inputId) {
+	public boolean checkId(String inputId) {
+		for (int i = 0; i < m.length; i++) {
+			if (m[i] != null) {
+				String id = m[i].getId();
+				if (id == inputId) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
-        return false;
-    }
+	public void insertMember(String id, String name, String password, String email, char gender, int age) {
+		Member mb = new Member(id, name, password, email, gender, age);
+		for (int i = 0; i < m.length; i++) {
+			if (m[i] == null) {
+				m[i] = mb;
+				return;
+			}
+		}
 
-    public void insertMember(String id, String name, String password, String email, String gender, int age) {
+	}
 
-    }
+	public Member searchId(String id) {
+		// 검색할 아이디를 사용자에게 입력 받고 입력 받은 데이터를
+		// mc의 searchId() 메소드의 매개변수로 넘김, 반환 값에 따라
+		// 검색 결과 없으면 “검색 결과가 없습니다.” 출력 후 mainMenu()로 돌아감
+		// 검색 결과가 있으면 “찾으신 회원 조회 결과입니다.” 출력 후
+		// 회원 검색 결과 출력
+		for (int i = 0; i < m.length; i++) {
+			String getId = m[i].getId();
+			
+			if(getId.equals(id))
+			{
+				return m[i]; // arr, brr, crr, ( arr -> id : dodle , name : 도헌 ..)
+			}
+		}
+		
+		return null;
+	}
 
-    public String searchId(String id) {
+	public Member[] searchName(String name) {
+		
+		Member[] mArr = new Member[10];
+		
+		for (int i = 0; i < m.length; i++) {
+			String getName = m[i].getName();
+			
+			if(getName.equals(name))	// arr, brr, crr 
+			{
+				mArr[i] = m[i];
+			}
+		}
+		return mArr;
+	}
 
-        return ""; 
-    }
+	public Member[] searchEmail(String email) {
 
-    public Member[] searchName(String name) {
+		return;
+	}
 
-        return;
-    }
+	public boolean updatePassword(String id, String password) {
 
-    public Member[] searchEmail(String email) {
+		return false;
+	}
 
-        return;
-    }
+	public boolean updateName(String id, String name) {
 
-    public boolean updatePassword(String id, String password) {
+		return false;
+	}
 
-        return false;
-    }
+	public boolean updateEmail(String id, String email) {
 
-    public boolean updateName(String id, String name) {
+		return false;
+	}
 
-        return false;
-    }
+	public boolean delete(String id) {
 
-    public boolean updateEmail(String id, String email) {
+		return false;
+	}
 
-        return false;
-    }
+	public void delete() {
 
-    public boolean delete(String id) {
+	}
 
-        return false;
-    }
+	public Member[] printAll() {
 
-    public void delete() {
-
-    }
-
-    public Member[] printAll() {
-
-        return;
-    }
+		for(int idx = 0 ; idx < m.length; idx++) {
+			if(m[idx] != null) {
+				return m;
+			}
+		}
+		return null;
+	}
 
 }
--- Homeworks3_ObjectArray/src/com/kh/hw/member/view/MemberMenu.java
+++ Homeworks3_ObjectArray/src/com/kh/hw/member/view/MemberMenu.java
@@ -3,28 +3,29 @@ package com.kh.hw.member.view;
 import java.util.Scanner;
 
 import com.kh.hw.member.controller.MemberController;
+import com.kh.hw.member.model.vo.Member;
 
 public class MemberMenu {
-   private Scanner sc = new Scanner(System.in);
-   private MemberController mc = new MemberController();
+	private Scanner sc = new Scanner(System.in);
+	private MemberController mc = new MemberController();
 
-   public MemberMenu() {
+	public MemberMenu() {
 
-   }
+	}
 
-   public void mainMenu() {
+	public void mainMenu() {
 
-      while(true) {
-      // MemberController(mc)클래스 안에 있는 멤버필드를 이용하여 최대 등록 가능한 회원 수가 몇 명인지 출력
-      int MaxMember = mc.SIZE;
-      System.out.println("최대 등록 가능한 회원 수는 " + MaxMember + "명입니다.");
+		while (true) {
+			// MemberController(mc)클래스 안에 있는 멤버필드를 이용하여 최대 등록 가능한 회원 수가 몇 명인지 출력
+			int MaxMember = mc.SIZE;
+			System.out.println("최대 등록 가능한 회원 수는 " + MaxMember + "명입니다.");
 
-      // 또한 현재 등록된 회원 수는 mc에 existMemberNum()의 반환 값으로 몇 명이 등록되어 있는지 출력
-      int NowMember = mc.existMemberNum();
-      System.out.println("현재 등록된 회원 수는 " + NowMember + "명입니다.");
+			// 또한 현재 등록된 회원 수는 mc에 existMemberNum()의 반환 값으로 몇 명이 등록되어 있는지 출력
+			int NowMember = mc.existMemberNum();
+			System.out.println("현재 등록된 회원 수는 " + NowMember + "명입니다.");
 
-      // 현재 등록된 회원 수에 따라 메인 메뉴를 다르게 출력
-      // 현재 등록된 회원 수가 10명이 아닐 때
+			// 현재 등록된 회원 수에 따라 메인 메뉴를 다르게 출력
+			// 현재 등록된 회원 수가 10명이 아닐 때
 //      1. 새 회원 등록 ➔ insertMember()
 //      2. 회원 검색 ➔ searchMember()
 //      3. 회원 정보 수정 ➔ updateMember()
@@ -32,84 +33,86 @@ public class MemberMenu {
 //      5. 모두 출력 ➔ printAll()
 //      9. 끝내기
 //      메뉴 번호 : 
-      if (NowMember < MaxMember) {
-         System.out.println("1. 새 회원 등록");
-         System.out.println("2. 회원 검색");
-         System.out.println("3. 회원 정보 수정");
-         System.out.println("4. 회원 삭제");
-         System.out.println("5. 모두 출력");
-         System.out.println("9. 끝내기");
-         System.out.print("메뉴 번호 : ");
-         int menuNum = sc.nextInt();
+			if (NowMember < MaxMember) {
+				System.out.println("1. 새 회원 등록");
+				System.out.println("2. 회원 검색");
+				System.out.println("3. 회원 정보 수정");
+				System.out.println("4. 회원 삭제");
+				System.out.println("5. 모두 출력");
+				System.out.println("9. 끝내기");
+				System.out.print("메뉴 번호 : ");
+				int menuNum = sc.nextInt();
 
-         switch (menuNum) {
-         case 1:
-            insertMember();
-            break;
+				switch (menuNum) {
+				case 1:
+					System.out.println("새 회원을 등록합니다.");
+					insertMember();
 
-         case 2:
-            searchMember();
-            break;
+					break;
 
-         case 3:
-            updateMember();
-            break;
+				case 2:
+					searchMember();
+					break;
 
-         case 4:
-            deleteMember();
-            break;
+				case 3:
+					updateMember();
+					break;
 
-         case 5:
-            printAll();
-            break;
+				case 4:
+					deleteMember();
+					break;
 
-         case 9:
-            System.out.println("프로그램을 종료합니다.");
-            return;
+				case 5:
+					printAll();
+					break;
 
-         default:
-            System.out.println("잘못 입력하셨습니다. 다시 입력해주세요");
-            break;
-         }
-      } else if (NowMember == MaxMember) {
-         System.out.println("회원 수가 모두 꽉 찼기 때문에 일부 메뉴만 오픈됩니다.");
-         System.out.println("2. 회원 검색");
-         System.out.println("3. 회원 정보 수정");
-         System.out.println("4. 회원 삭제");
-         System.out.println("5. 모두 출력");
-         System.out.println("9. 끝내기");
-         int menuNum = sc.nextInt();
-         
-         switch (menuNum) {
+				case 9:
+					System.out.println("프로그램을 종료합니다.");
+					return;
 
-         case 2:
-            searchMember();
-            break;
+				default:
+					System.out.println("잘못 입력하셨습니다. 다시 입력해주세요");
+					break;
+				}
+			} else if (NowMember == MaxMember) {
+				System.out.println("회원 수가 모두 꽉 찼기 때문에 일부 메뉴만 오픈됩니다.");
+				System.out.println("2. 회원 검색");
+				System.out.println("3. 회원 정보 수정");
+				System.out.println("4. 회원 삭제");
+				System.out.println("5. 모두 출력");
+				System.out.println("9. 끝내기");
+				int menuNum = sc.nextInt();
 
-         case 3:
-            updateMember();
-            break;
+				switch (menuNum) {
 
-         case 4:
-            deleteMember();
-            break;
+				case 2:
+					searchMember();
+					break;
 
-         case 5:
-            printAll();
-            break;
+				case 3:
+					updateMember();
+					break;
 
-         case 9:
-            System.out.println("프로그램을 종료합니다.");
-            return;
+				case 4:
+					deleteMember();
+					break;
 
-         default:
-            System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
-            break;
-         }
-      }
+				case 5:
+					printAll();
+					break;
 
-      }
-      // 현재 등록된 회원 수가 10명일 때
+				case 9:
+					System.out.println("프로그램을 종료합니다.");
+					return;
+
+				default:
+					System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
+					break;
+				}
+			}
+
+		}
+		// 현재 등록된 회원 수가 10명일 때
 //      회원 수가 모두 꽉 찼기 때문에 일부 메뉴만 오픈됩니다.
 //      2. 회원 검색 ➔ searchMember()
 //      3. 회원 정보 수정 ➔ updateMemner()
@@ -117,62 +120,133 @@ public class MemberMenu {
 //      5. 모두 출력 ➔ printAll()
 //      9. 끝내기
 //      메뉴 번호 :
-      // 두 경우 모두 끝내기를 누르면 “프로그램을 종료합니다” 출력 후
-      // 프로그램 종료하고 메뉴 번호에 없는 번호를 누를 시
-      // “잘못 입력하셨습니다. 다시 입력해주세요.” 출력 후 메뉴 반복
+		// 두 경우 모두 끝내기를 누르면 “프로그램을 종료합니다” 출력 후
+		// 프로그램 종료하고 메뉴 번호에 없는 번호를 누를 시
+		// “잘못 입력하셨습니다. 다시 입력해주세요.” 출력 후 메뉴 반복
 
-   }
+	}
 
-   public void insertMember() {
+	public void insertMember() {
+		// 회원 등록에 필요한 아이디, 이름, 비밀번호, 이메일, 성별, 나이를 사용자에게 직접 입력 받되 아이디의 경우 기존 회원의 아이디와
+		// 중복되지 않도록 mc의 checkId()메소드를 통해서 중복 검사를 함
+		// 중복 검사를 한 결과 값에 따라 중복 값이 없으면 다음으로 넘어가고 있으면 “중복된 아이디입니다. 다시 입력해주세요.”라는 문구를 출력 후
+		// 다시 아이디를 받도록 함
+		String id = "";
+		while (true) {
+			System.out.print("아이디 : ");
+			id = sc.next();
+			boolean check = mc.checkId(id);
 
-   }
+			if (check == true) {
+				System.out.println("중복된 아이디입니다. 다시 입력해주세요");
 
-   public void searchMember() {
+			} else {
+				break;
+			}
+		}
 
-   }
+		System.out.print("이름 : ");
+		String name = sc.next();
+		System.out.print("비밀번호 : ");
+		String password = sc.next();
+		System.out.print("이메일 : ");
+		String email = sc.next();
 
-   public void searchId() {
+		// 성별의 경우 M이나 m, F나 f가 아니면 “성별을 다시 입력하세요.”를 출력 후 다시 성별을 입력하도록 함
+		// 아이디부터 나이까지 모든 데이터를 받았으면 mc의 insertMember메소드의 매개변수로 넘김
+		char gender = ' ';
+		while (true) {
+			System.out.print("성별(M/F) : ");
+			gender = sc.next().charAt(0);
+			if (!(gender == 'm' || gender == 'M' || gender == 'f' || gender == 'F')) {
+				System.out.println("성별을 다시 입력하세요.");
+			}else {
+				break;
+			}
+		}
+		System.out.print("나이 : ");
+		int age = sc.nextInt();
+		mc.insertMember(id, name, password, email, gender, age);
+	}
 
-   }
+	public void searchMember() {
+//		1. 아이디로 검색하기 ➔ searchId()
+//		2. 이름으로 검색하기 ➔ searchName()
+//		3. 이메일로 검색하기 ➔ searchEmail()
+//		9. 메인으로 돌아가기
+//		메뉴 번호 :
+		// 메뉴 번호에 따라 각 메소드로 이동되며 9번을 입력했을 경우
+		// “메인으로 돌아갑니다.” 출력 후 mainMenu()로 돌아감
+		// 메뉴 번호를 잘못 입력했을 경우 “잘못 입력하셨습니다.” 출력 후
+		// mainMenu()로 돌아감
+		
+		// switch
+	}
 
-   public void searchName() {
+	public void searchId(String id) {
+		Member result = mc.searchId(id);
+		
+		if(result == null){
+			System.out.println("검색 결과가 없습니다.");
+		}
+		else {
+			printOne(result);
+		}
+		
+	}
 
-   }
+	public void searchName(String name) {
+		Member[] mArr = mc.searchName(name);
+		printAll(mArr);
+	}
 
-   public void searchEmail() {
+	public void searchEmail() {
 
-   }
+	}
 
-   public void updateMember() {
+	public void updateMember() {
 
-   }
+	}
 
-   public void updatePassword() {
+	public void updatePassword() {
 
-   }
+	}
 
-   public void updateName() {
+	public void updateName() {
 
-   }
+	}
 
-   public void updateEmail() {
+	public void updateEmail() {
 
-   }
+	}
 
-   public void deleteMember() {
+	public void deleteMember() {
 
-   }
+	}
 
-   public void deleteOne() {
+	public void deleteOne() {
 
-   }
+	}
 
-   public void deleteAll() {
+	public void deleteAll() {
 
-   }
+	}
+	
+	public void printOne(Member result) {
+		System.out.println(result.inform());
+	}
 
-   public void printAll() {
+	public void printAll() {
+		Member[] m = mc.printAll();
+		
+		if(m == null)
+			System.out.println("0명 입니다.");
+		
+		for(int idx = 0 ; idx < m.length; idx++) {
 
-   }
+			if(m[idx] != null)
+				System.out.println(m[idx].inform());
+		}
+	}
 
 }
